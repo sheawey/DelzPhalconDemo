caff1b8e7da6547104a6484e917b33887306f7a0
徐威 <sheawey@gmail.com>
2017-10-19 17:52:41 +0800

    增加IndexController demo

---
+++ src/App/Controller/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Delz\Phalcon\Mvc\Controller\Controller;
+
+/**
+ * @package App\Controller
+ */
+class IndexController extends  Controller
+{
+    public function indexAction()
+    {
+        echo 'Hello';
+    }
+}
